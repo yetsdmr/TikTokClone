924e481c9cbf2ba1280344b75793b3007861e53e
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-11-13 16:12:15 +0300

    Initial Commit

---
+++ TikTokClone/TikTokCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokCloneApp.swift
+//  TikTokClone
+//
+//  Created by Yunus Emre Taşdemir on 13.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct TikTokCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
